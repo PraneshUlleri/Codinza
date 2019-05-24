da524939b186adfc2c69dc0093976d875760a4db
Pranesh <ipraneshulleri@gmail.com>
2019-05-24 19:07:38 +0530

    added the file in telegram

--- Day 4/telegramfile.js
+++ Day 4/telegramfile.js
@@ -1,1 +1,23 @@
-telegramfile.js
+var telegram = require('telegram-bot-api');
+var file = require('fs')
+
+var api = new telegram({
+        token: '872363712:AAG_QyQauhEmIo6Q2N50YrSCtuExT11y9JA',
+        updates:{enabled:true}
+});
+
+
+api.on('message',function(a){
+
+console.log(a.text);
+
+if(a.text=="hi")
+api.sendMessage({
+	chat_id: a.chat.id,
+ 	text:"hello"
+})
+file.appendFile("temp.txt", a.text+ "|-| \n", (err) => {
+  if (err) console.log(err);
+  console.log("Successfully Written to File.");
+});
+});
